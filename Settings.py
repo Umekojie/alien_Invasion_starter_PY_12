e9ba3ba5fca4331e90d1740697093b52c86658d2
Josephine Cattell <josieohio7967@gmail.com>
2026-04-12 22:32:03 -0400

    Adding Inital code and changing background and ship images

---
+++ Settings.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+class Settings:
+  def __init__(self):
+        self.name: str ='Alien Invasion'
+        self.screen_w = 1200
+        self.screen_h = 800 
+        self.FPS = 60
+        self.bg_file = Path.cwd()/'Assets'/'images'/'background.png'
+
+        self.ship_file = Path.cwd()/'Assets'/'images'/'Ladyship.png'
+        self.ship_w = 40
+        self.ship_h = 60
+        self.ship_speed = 5
+
+        self.bullet_file = Path.cwd()/'Assets'/'images'/'rosebullet.png'
+        self.laser_sound = Path.cwd()/'Assets'/'sounds'/'laser.mp3'
+        self.bullet_speed = 7
+        self.bullet_w = 25
+        self.bullet_h = 80
+        self.bullet_amount = 5
+        
--- alien_invasion.py
+++ alien_invasion.py
@@ -4,8 +4,91 @@ Customizing core player assets and Background
 This is based on the Eric Matthes 'Python Crash Course' walkthrough and  https://github.com/RedBeard41/alien_Invasion_starter.git
 4/12/26"""
 
+from pathlib import Path
 import sys
 import pygame
-from pathlib import Path
+from Settings import Settings
+from ship import Ship
+from arsenal import ShipArsenal
+
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        
+        self.screen = pygame.display.set_mode((self.settings.screen_w, self.settings.screen_h))
+        pygame.display.set_caption(self.settings.name)
+        
+        
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg, (self.settings.screen_w, self.settings.screen_h))
+        
+        
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+        pygame.mixer.init()
+        # Change 'sounds' to 'sound'
+        
+        self.laser_sound = pygame.mixer.Sound(str(self.settings.laser_sound))
+        self.laser_sound.set_volume(0.7)
+        # Adding game feature
+        
+       
+        self.ship = Ship(self, ShipArsenal(self))
+
+
+    def run_game(self)-> None:
+        # game loop
+        while self.running:
+            self._check_events()
+                    #draw the ship then background
+            self.ship.update()
+        
+            self._update_screen()
+            self.clock.tick(self.settings.FPS)
+
+    def _update_screen(self):
+        self.screen.blit(self.bg,(0,0))
+        self.ship.draw()
+        pygame.display.flip()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+    
+    
+    def _check_keyup_events(self, event) -> None:
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+
+
+    def _check_keydown_events(self, event) -> None:
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:        
+            if self.ship.fire():
+                self.laser_sound.play()
+                #self.laser_sound.fadeout(250)
+                #play laser sound
+            
+        elif event.key == pygame.K_q:
+            self.ship.moving_left = True
+            pygame.quit()
+            sys.exit()
+    
 if __name__ == '__main__':
-    pass
+    ai = AlienInvasion()
+    ai.run_game()
