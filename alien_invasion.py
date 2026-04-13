c5684eb6ab778e5888a5eb535f23674cc515d4dc
Josephine Cattell <josieohio7967@gmail.com>
2026-04-12 20:53:49 -0400

    Add documentation

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,11 @@
+"""Alien Invasion Game
+Josephine Cattell
+Customizing core player assets and Background
+This is based on the Eric Matthes 'Python Crash Course' walkthrough and  https://github.com/RedBeard41/alien_Invasion_starter.git
+4/12/26"""
+
 import sys
 import pygame
-
+from pathlib import Path
 if __name__ == '__main__':
     pass
