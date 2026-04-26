afa813fef1aff86666b5635620883eec0d37fe01
Josephine Cattell <josieohio7967@gmail.com>
2026-04-25 20:53:19 -0400

    P3 Added buttion file and button settings,as well as added collision

---
+++ button.py
@@ -0,0 +1,35 @@
+import pygame.font
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+
+
+class Button:
+
+    def __init__(self,game: 'AlienInvasion',msg)->None:
+        self.game = game
+        self.screen = game.screen
+        self.boundries = self.screen.get_rect()
+        self.settings = game.settings
+        self.font = pygame.font.Font (self.settings.font_file, 
+            self.settings.button_font_size)
+        # button size
+        self.rect = pygame.Rect(0,0,self.settings.button_w, self.settings.button_h)
+        self.rect.center = self.boundries.center
+        self._prep_msg(msg)
+
+    def _prep_msg (self, msg)-> None:
+        self.msg_image = self.font.render(msg, True, self.settings.text_color,None)
+# antialiasing 
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        self.screen.fill(self.settings.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
+# check mouse collision
+    def check_click(self, mouse_pos):
+        return self.rect.collidepoint(mouse_pos)
+    
